8fed526a760172cb2d586be0de868ba328c64f52
yangfengzzz <yangfengzzz@hotmail.com>
2023-11-18 18:47:10 +0800

    add test and benchmark framework

---
+++ benchmark/main.cpp
@@ -0,0 +1,13 @@
+//  Copyright (c) 2023 Feng Yang
+//
+//  I am making my contributions/submissions to this project solely in my
+//  personal capacity and am not conveying any rights to any intellectual
+//  property of any third parties.
+
+#include <benchmark/benchmark.h>
+
+int main(int argc, char **argv) {
+    ::benchmark::Initialize(&argc, argv);
+
+    ::benchmark::RunSpecifiedBenchmarks();
+}
---
+++ unittest/main.cpp
@@ -0,0 +1,12 @@
+//  Copyright (c) 2023 Feng Yang
+//
+//  I am making my contributions/submissions to this project solely in my
+//  personal capacity and am not conveying any rights to any intellectual
+//  property of any third parties.
+
+#include <gtest/gtest.h>
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
